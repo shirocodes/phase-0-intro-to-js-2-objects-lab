fe6f7d31ba88dfe49c9499299e7e7eda3b741039
shirocodes <ishirapt@gmail.com>
2025-02-28 23:18:05 +0300

    Manipulated objects

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution in this file!
+const employee = {
+    name: "Sam",
+    streetAddress: "Sky",
+};
+
+function updateEmployeeWithKeyAndValue(employee,key, value) {
+    const newEmployee = {...employee, [key]: value};
+    return newEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee3 = {... employee};
+    delete newEmployee3[key];
+    return newEmployee3;
+}
+
+
+
+
+
+
+
+
